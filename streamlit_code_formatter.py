05c34fd175cb2c5b31aa5b466859aea7b69bcf61
swsmr <61564538+swsmr@users.noreply.github.com>
2024-03-09 19:53:21 +0100

    Update streamlit_code_formatter.py
    Add headers.

--- streamlit_code_formatter.py
+++ streamlit_code_formatter.py
@@ -4,17 +4,20 @@ import black
 
 import streamlit as st
 
-st.set_page_config(page_title="Online Python code auto-formatter", layout="wide")
-st.title('Online Python code auto-formatter')
-    
+st.set_page_config(page_title="Python Code Formatter", layout="wide")
+st.title('Python Code Formatter')
+
+st.header('Input code')
 code_in = st.text_area("Input code")
 if code_in:
+    st.header('Formatter')
     formatter = st.radio("Formatter", ['yapf', 'autopep8', 'black'])
     if formatter == 'yapf':
-        style_config = st.selectbox("yapf style", ['pep8', 'google', 'yapf', 'facebook'], index=1)
+        style_config = st.selectbox("Select yapf style", ['pep8', 'google', 'yapf', 'facebook'], index=1)
         code_out, _ = FormatCode(code_in, style_config=style_config)
     elif formatter == 'autopep8':
         code_out = autopep8.fix_code(code_in)
     elif formatter == 'black':
         code_out = black.format_str(code_in, mode=black.FileMode())
+    st.header('Formatted code')
     st.code(code_out, language='python')
