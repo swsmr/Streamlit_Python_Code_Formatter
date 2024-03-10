0b165e95ffcbd1e5d641b06ca9d2689696e0e06e
swsmr <61564538+swsmr@users.noreply.github.com>
2024-03-10 18:44:08 +0100

    Update streamlit_code_formatter.py

--- streamlit_code_formatter.py
+++ streamlit_code_formatter.py
@@ -25,10 +25,7 @@ if code_in:
         code_out = autopep8.fix_code(code_in)
     elif formatter == 'black':
         # code_out = black.format_str(code_in, mode=black.FileMode())
-        try:
-            code_out = black.format_file_contents(code_in, fast=False, mode=black.FileMode())
-        except: # NothingChanged:
-            code_out = code_in
+        code_out = black.format_file_contents(code_in, fast=False, mode=black.FileMode())
 
     st.header('Formatted code')
     st.code(code_out, language='python', line_numbers=st.sidebar.toggle("Display line numbers?", value=False))
